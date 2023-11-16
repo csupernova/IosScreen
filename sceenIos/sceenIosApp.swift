c1cf8eea5f05fc452afcd77d3baa34ff436f812b
Dmitry <mr.kargapolov@gmail.com>
2023-11-16 10:37:16 -0800

    Initial Commit

---
+++ sceenIos/sceenIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sceenIosApp.swift
+//  sceenIos
+//
+//  Created by Dmitry on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct sceenIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
